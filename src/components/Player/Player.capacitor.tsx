bc457872364512dd7f3098a54a0320af4665a7e3
Christiaan Scheermeijer <christiaan@videodock.com>
2023-10-11 14:22:50 +0200

    feat: implement capacitor-video-player

--- src/components/Player/Player.capacitor.tsx
+++ src/components/Player/Player.capacitor.tsx
@@ -1,34 +1,18 @@
 import React, { useEffect } from 'react';
-import { JWPlayer } from 'jwplayer-capacitor';
+import { CapacitorVideoPlayer } from 'capacitor-video-player';
 
 import styles from './Player.module.scss';
 import type { Props } from './Player';
 import type { PlaylistItem } from '#types/playlist';
 
 async function initPlayer (item: PlaylistItem) {
-  await JWPlayer.initialize({
-    iosLicenseKey: '...',
-    androidLicenseKey: '...',
-  });
-
-  await JWPlayer.create({
-    nativeConfiguration: {
-      width: 100,
-      height: 100,
-      x: 0,
-      y: 0,
-      forceFullScreen: true,
-      playlist: [
-        {
-          file: item.sources[0].file,
-          image: item.image,
-          title: item.title,
-          description: item.description,
-          starttime: 0,
-          captions: [],
-        },
-      ],
-    },
+  await CapacitorVideoPlayer.initPlayer({
+    mode: 'fullscreen',
+    url: item.sources[0].file,
+    playerId: 'player',
+    componentTag: 'div',
+    title: item.title,
+    artwork: item.image,
   });
 }
 
@@ -39,7 +23,7 @@ const Player: React.FC<Props> = ({
     initPlayer(item);
 
     return () => {
-      JWPlayer.remove();
+      CapacitorVideoPlayer.exitPlayer();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
