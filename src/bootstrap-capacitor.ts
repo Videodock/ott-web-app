c40d5b7f6de8da2de8531cca351dd0e2af39901b
Christiaan Scheermeijer <christiaan@videodock.com>
2023-10-12 16:57:52 +0200

    fix: android statusbar and safe area

--- src/bootstrap-capacitor.ts
+++ src/bootstrap-capacitor.ts
@@ -1,10 +1,50 @@
-import { StatusBar, Style} from '@capacitor/status-bar';
-import { Share} from '@capacitor/share';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Share } from '@capacitor/share';
+import { SafeArea, SafeAreaInsets } from 'capacitor-plugin-safe-area';
+
+const applySafeAreaVariables = (insets: SafeAreaInsets) => {
+  for (const [key, value] of Object.entries(insets.insets)) {
+    document.documentElement.style.setProperty(`--safe-area-${key}`, `${value}px`);
+  }
+};
+
+async function initializeSafeArea() {
+  // initial safe area
+  const insets = await SafeArea.getSafeAreaInsets();
+
+  applySafeAreaVariables(insets);
+
+  // listen for changes
+  await SafeArea.addListener('safeAreaChanged', async ({ insets }) => {
+    // this event is triggered to soon and the insets still reflect the before rotation values
+    // so, we wait for a bit
+    await new Promise((resolve) => setTimeout(resolve, 200));
+
+    let i = 5;
+    while (i--) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+
+      const newInsets = await SafeArea.getSafeAreaInsets();
+
+      if (newInsets.insets.top !== insets.top) {
+        applySafeAreaVariables(newInsets);
+        break;
+      }
+    }
+  });
+}
 
 async function bootstrap() {
   console.log('Bootstrap capacitor app');
 
   await StatusBar.setStyle({ style: Style.Dark });
+
+  if (Capacitor.getPlatform() === 'android') {
+    await StatusBar.setOverlaysWebView({ overlay: true });
+  }
+
+  await initializeSafeArea();
 
   const canShare = await Share.canShare();
 
@@ -12,6 +52,5 @@ async function bootstrap() {
   // @ts-ignore
   navigator.share = canShare ? Share.share : undefined;
 }
-
 
 bootstrap();
