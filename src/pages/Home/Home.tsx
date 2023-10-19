16a9106d4c245043b2d5dda1d933a1df89333b10
Melissa 't Hart <melissa@videodock.com>
2023-10-19 08:54:15 +0200

    feat(a11y): correct heading structure

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import shallow from 'zustand/shallow';
+import { useTranslation } from 'react-i18next';
+
+import '../../styles/accessibility.scss';
 
 import { useConfigStore } from '#src/stores/ConfigStore';
 import type { Content } from '#types/Config';
@@ -8,8 +11,14 @@ import ShelfList from '#src/containers/ShelfList/ShelfList';
 const Home = () => {
   const { config } = useConfigStore(({ config, accessModel }) => ({ config, accessModel }), shallow);
   const content: Content[] = config?.content;
+  const { t } = useTranslation('common');
 
-  return <ShelfList rows={content} />;
+  return (
+    <>
+      <h1 className="hideUntilFocus">{t('home')}</h1>
+      <ShelfList rows={content} />
+    </>
+  );
 };
 
 export default Home;
