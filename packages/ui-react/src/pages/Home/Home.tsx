a2e907b8e6359f5e225290264305bbe3f94b2b2f
Melissa 't Hart <melissa@videodock.com>
2023-12-12 18:22:29 +0100

    refactor(home): add home scss file

--- packages/ui-react/src/pages/Home/Home.tsx
+++ packages/ui-react/src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import type { Content } from '@jwp/ott-common/types/config';
 import { useConfigStore } from '@jwp/ott-common/src/stores/ConfigStore';
 import { useTranslation } from 'react-i18next';
 
-import '../../styles/accessibility.scss';
+import styles from './Home.module.scss';
 
 import ShelfList from '../../containers/ShelfList/ShelfList';
 
@@ -15,7 +15,7 @@ const Home = () => {
 
   return (
     <>
-      <h1 className="hideUntilFocus">{t('home')}</h1>
+      <h1 className={styles.hideUntilFocus}>{t('home')}</h1>
       <ShelfList rows={content} />
     </>
   );
