66640239fcd58690bddc9e8645159caa0df11430
Rachid <rachid.ang@sounds.global>
2024-01-08 16:16:42 +0100

    'refactor(tests): update unit tests and snapshot for packages/ui-react/src/components/LanguageMenu/LanguageMenu.test.tsx

--- packages/ui-react/src/components/LanguageMenu/LanguageMenu.test.tsx
+++ packages/ui-react/src/components/LanguageMenu/LanguageMenu.test.tsx
@@ -1,52 +1,76 @@
-import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import type { LanguageDefinition } from 'packages/common/types/i18n';
 
 import { renderWithRouter } from '../../../test/utils';
 
 import LanguageMenu from './LanguageMenu';
 
 const languages = [
-  {
-    displayName: 'English',
-    code: 'en',
-  },
-  {
-    displayName: 'español',
-    code: 'es',
-  },
+  { displayName: 'English', code: 'en' },
+  { displayName: 'español', code: 'es' },
 ];
 
-describe('<LanguageMenu>', () => {
-  test('renders and matches snapshot', () => {
-    const currentLanguage = languages[1];
-    const { container } = renderWithRouter(<LanguageMenu languages={languages} currentLanguage={currentLanguage} onClick={() => undefined} />);
+const onClickCb = vi.fn();
+const openLanguageMenuCb = vi.fn();
+const closeLanguageMenuCb = vi.fn();
 
+describe('<LanguageMenu>', () => {
+  afterEach(() => {
+    onClickCb.mockClear();
+    openLanguageMenuCb.mockClear();
+    closeLanguageMenuCb.mockClear();
+    cleanup();
+  });
+
+  const renderLanguageMenu = (currentLanguage: LanguageDefinition, languageMenuOpen: boolean) => {
+    return renderWithRouter(
+      <LanguageMenu
+        languages={languages}
+        currentLanguage={currentLanguage}
+        onClick={onClickCb}
+        languageMenuOpen={languageMenuOpen}
+        openLanguageMenu={openLanguageMenuCb}
+        closeLanguageMenu={closeLanguageMenuCb}
+      />,
+    );
+  };
+
+  test('renders and matches snapshot', () => {
+    const { container } = renderLanguageMenu(languages[0], false);
     expect(container).toMatchSnapshot();
   });
 
-  test('shows all languages in the menu when button is clicked and calls the onClick callback with the correct language code', () => {
-    const currentLanguage = languages[1];
-    const callback = vi.fn();
-
-    const { getByRole, container } = renderWithRouter(<LanguageMenu languages={languages} currentLanguage={currentLanguage} onClick={callback} />);
-    const languageMenuButton = getByRole('button');
-
-    // Simulate button click for accessiblity
-    fireEvent.click(languageMenuButton);
+  test('Renders the correct items in the LanguageMenu', () => {
+    const { container } = renderLanguageMenu(languages[0], true);
     const languagePanel = container.querySelector('#language-panel');
     expect(languagePanel).toBeInTheDocument();
 
     const languageMenuItems = languagePanel?.querySelectorAll('li');
-    expect(languageMenuItems).toHaveLength(languages.length);
 
-    // check if all languages are displayed correctly
+    expect(languageMenuItems).toHaveLength(languages.length);
     languages.forEach((lang, index) => {
       expect(languageMenuItems?.[index]).toHaveTextContent(lang.displayName);
     });
+  });
 
-    // ignore line since the value is hardcoded above.
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    fireEvent.click(languageMenuItems![0]);
-    expect(callback).toHaveBeenCalledWith(languages[0].code);
+  test('Check if the correct language item is selected onclick', () => {
+    const { container } = renderLanguageMenu(languages[0], true);
+    const languageMenuItems = container.querySelectorAll('#language-panel li');
+    fireEvent.click(languageMenuItems[0]);
+    expect(onClickCb).toHaveBeenCalledWith(languages[0].code);
+  });
+
+  test('Calls openLanguageCb when onClick is called and menu is closed', () => {
+    const { getByRole } = renderLanguageMenu(languages[0], false);
+    const languageMenuButton = getByRole('button');
+    fireEvent.click(languageMenuButton);
+    expect(openLanguageMenuCb).toHaveBeenCalled();
+  });
+
+  test('Calls closeLanguageMenuCb when onClick is called and menu is open', () => {
+    const { getByRole } = renderLanguageMenu(languages[0], true);
+    const languageMenuButton = getByRole('button');
+    fireEvent.click(languageMenuButton);
+    expect(closeLanguageMenuCb).toHaveBeenCalled();
   });
 });
