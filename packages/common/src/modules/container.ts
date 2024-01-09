d5aa3ff6f54deec7d914a4a0736e8a265ccec36f
Christiaan Scheermeijer <christiaan@videodock.com>
2024-01-09 14:37:37 +0100

    fix: log getNamedModule initialization error

--- packages/common/src/modules/container.ts
+++ packages/common/src/modules/container.ts
@@ -1,6 +1,7 @@
 import { Container, injectable, type interfaces, inject } from 'inversify';
 
 import type { IntegrationType } from '../../types/config';
+import { logDev } from '../utils/common';
 
 export const container = new Container({ defaultScope: 'Singleton', skipBaseClassChecks: true });
 
@@ -32,9 +33,15 @@ export function getNamedModule<T>(constructorFunction: interfaces.ServiceIdentif
 
     return module;
   } catch (err: unknown) {
-    if (required) {
-      throw new Error(`Service not found '${String(constructorFunction)}' with name '${integration}'`);
+    if (err instanceof Error && err.message.includes('No matching bindings found')) {
+      if (required) {
+        throw new Error(`Service not found '${String(constructorFunction)}' with name '${integration}'`);
+      }
+
+      return;
     }
+
+    logDev('Error caught while initializing service', err);
   }
 }
 
