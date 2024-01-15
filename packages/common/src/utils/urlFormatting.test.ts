4e32d46d511fab39cc457efa01aef55b4262ea99
Roy Schut <roy@videodock.com>
2024-01-15 11:08:55 +0100

    refactor(tests): add unit test for redirect url

--- packages/common/src/utils/urlFormatting.test.ts
+++ packages/common/src/utils/urlFormatting.test.ts
@@ -11,4 +11,15 @@ describe('createUrl', () => {
 
     expect(url).toEqual('/test?existing-param=1&foo=bar');
   });
+
+  test('valid url from a path including params, removing one with a query param', async () => {
+    const url = createURL('/test?existing-param=1', { [`existing-param`]: null });
+
+    expect(url).toEqual('/test');
+  });
+  test('valid redirect url from a location including params, query params', async () => {
+    const url = createURL('https://app-preview.jwplayer.com/?existing-param=1&foo=bar', { u: 'payment-method-success' });
+
+    expect(url).toEqual('https://app-preview.jwplayer.com/?existing-param=1&foo=bar&u=payment-method-success');
+  });
 });
