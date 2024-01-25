1971a7ed47143ca53ca0bbebf9edde09aac7192e
Roy Schut <roy@videodock.com>
2024-01-25 07:41:35 -0300

    refactor(project): remove obsolete import

--- packages/ui-react/src/modules/container.ts
+++ packages/ui-react/src/modules/container.ts
@@ -1,5 +1,3 @@
-import 'reflect-metadata'; // include once in the app for inversify (see: https://github.com/inversify/InversifyJS/blob/master/README.md#-installation)
-
 import { Container } from 'inversify';
 
 const container = new Container({ defaultScope: 'Singleton', skipBaseClassChecks: true });
