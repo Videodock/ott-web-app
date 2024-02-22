04d4710fecca981a9e0ffd05a9a53402a8fb026b
Mike van Veenhuijzen <mike@videodock.com>
2024-02-22 11:42:22 +0100

    chore: footer without self invoking function & trim content

--- packages/ui-react/src/components/Footer/Footer.tsx
+++ packages/ui-react/src/components/Footer/Footer.tsx
@@ -11,23 +11,13 @@ type Props = {
 };
 
 const Footer: React.FC<Props> = ({ text }) => {
-  const chunks = text.split('|');
+  const chunks = text.split('|').map((chuck) => chuck.trim());
+  const footerContent = chunks.map((value, index) => <MarkdownComponent key={index} markdownString={value} inline tag={chunks.length > 1 ? 'li' : 'div'} />);
+  const wrapper = chunks.length > 1 ? <ul className={styles.list}>{footerContent}</ul> : footerContent;
 
   return (
     // The extra style below is just to fix the footer on mobile when the dev selector is shown
-    <footer className={classNames(styles.footer, { [styles.testFixMargin]: IS_DEVELOPMENT_BUILD })}>
-      {(() => {
-        const footerContent = chunks.map((value, index) => (
-          <MarkdownComponent key={index} markdownString={value} inline tag={chunks.length > 1 ? 'li' : 'div'} />
-        ));
-
-        if (chunks.length > 1) {
-          return <ul className={styles.list}>{footerContent}</ul>;
-        } else {
-          return footerContent;
-        }
-      })()}
-    </footer>
+    <footer className={classNames(styles.footer, { [styles.testFixMargin]: IS_DEVELOPMENT_BUILD })}>{wrapper}</footer>
   );
 };
 
