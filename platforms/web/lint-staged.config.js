a8ca98615c0d359b84f424ea13a98fb76216f4fc
Melissa 't Hart <melissa@videodock.com>
2024-03-07 17:29:27 +0100

    chore(project): update packages to latest version

--- platforms/web/lint-staged.config.js
+++ platforms/web/lint-staged.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   '*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write', 'vitest related --run'],
   '*.scss': ['stylelint --fix'],
   '*.{ts,tsx}': [() => 'tsc --pretty --noEmit'],
