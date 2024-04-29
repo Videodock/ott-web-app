9b35f023089af99c4991b208f6df91a2ef1d551b
Mike van Veenhuijzen <mike@videodock.com>
2024-04-29 17:33:46 +0200

    chore: code cleanup

--- platforms/web/test-e2e/tests/accessibility_test.ts
+++ platforms/web/test-e2e/tests/accessibility_test.ts
@@ -5,18 +5,18 @@ Feature('accessbility');
 Scenario('Homepage WCAG compliant', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/');
-  I.checkA11y(undefined);
+  I.checkA11y();
 });
 
 Scenario('Video detail page WCAG compliant', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
-  I.checkA11y(undefined);
+  I.checkA11y();
 });
 
 Scenario('Playlist page WCAG compliant', async ({ I }) => {
   await I.amOnPage('/p/dGSUzs9o/'); // "Films" page
-  I.checkA11y(undefined);
+  I.checkA11y();
 });
 
 Scenario('Video detail inline page WCAG compliant', async ({ I }) => {
@@ -29,11 +29,11 @@ Scenario('Video detail inline page WCAG compliant', async ({ I }) => {
 // Scenario('Signup modal WCAG compliant', async ({ I }) => {
 //   I.useConfig(testConfigs.jwpAuth);
 //   I.amOnPage('/?u=create-account');
-//   I.checkA11y(undefined);
+//   I.checkA11y();
 // });
 
 // Scenario('Signin modal WCAG compliant', async ({ I }) => {
 //   I.useConfig(testConfigs.jwpAuth);
 //   I.amOnPage('/?u=login');
-//   I.checkA11y(undefined);
+//   I.checkA11y();
 // });
