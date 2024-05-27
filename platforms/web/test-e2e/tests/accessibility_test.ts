41e2c389827fdc55f87f3fde197700e5e0251237
Mike van Veenhuijzen <mike@videodock.com>
2024-05-27 14:45:28 +0200

    chore: improve e2e stability for accessibility

--- platforms/web/test-e2e/tests/accessibility_test.ts
+++ platforms/web/test-e2e/tests/accessibility_test.ts
@@ -2,17 +2,21 @@ import { testConfigs } from '@jwp/ott-testing/constants';
 
 Feature('accessbility');
 
+const disableRetryFailedStep = (test: { disableRetryFailedStep: boolean }): void => {
+  test.disableRetryFailedStep = true;
+};
+
 Scenario('Homepage WCAG compliant', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/');
   I.checkA11y();
-});
+}).config(disableRetryFailedStep);
 
 Scenario('Video detail page WCAG compliant', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
   I.checkA11y();
-});
+}).config(disableRetryFailedStep);
 
 Scenario('Playlist page WCAG compliant', async ({ I }) => {
   await I.amOnPage('/p/dGSUzs9o/'); // "Films" page
@@ -22,14 +26,14 @@ Scenario('Playlist page WCAG compliant', async ({ I }) => {
       // { id: 'aria-valid-attr-value', selector: '[role="gridcell"]' },
     ],
   });
-});
+}).config(disableRetryFailedStep);
 
 Scenario('Video detail inline page WCAG compliant', async ({ I }) => {
   // Fails because of role="image" with aria-label
   I.useConfig(testConfigs.inlinePlayer);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
   I.checkA11y();
-});
+}).config(disableRetryFailedStep);
 
 // Scenario('Signup modal WCAG compliant', async ({ I }) => {
 //   I.useConfig(testConfigs.jwpAuth);
