80314ae17b13ea813fa2ec2b9018eae4634fd59c
Melissa 't Hart <melissa@videodock.com>
2024-05-29 17:14:03 +0200

    fix(a11y): aria-prohibited-attr rule and add account pages tests

--- platforms/web/test-e2e/tests/accessibility_test.ts
+++ platforms/web/test-e2e/tests/accessibility_test.ts
@@ -1,24 +1,24 @@
 import { testConfigs } from '@jwp/ott-testing/constants';
 
-Feature('accessbility');
+Feature('Accessibility');
 
 const disableRetryFailedStep = (test: { disableRetryFailedStep: boolean }): void => {
   test.disableRetryFailedStep = true;
 };
 
-Scenario('Homepage WCAG compliant', async ({ I }) => {
+Scenario('WCAG compliant - Home Page', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/');
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
-Scenario('Video detail page WCAG compliant', async ({ I }) => {
+Scenario('WCAG compliant - Video Detail Page', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
-Scenario('Playlist page WCAG compliant', async ({ I }) => {
+Scenario('WCAG compliant - Playlist Page', async ({ I }) => {
   await I.amOnPage('/p/dGSUzs9o/'); // "Films" page
   I.checkA11y(null, {
     ignore: [
@@ -28,14 +28,37 @@ Scenario('Playlist page WCAG compliant', async ({ I }) => {
   });
 }).config(disableRetryFailedStep);
 
-Scenario('Video detail inline page WCAG compliant', async ({ I }) => {
+Scenario('WCAG compliant - Video Detail Inline Page', async ({ I }) => {
   // Fails because of role="image" with aria-label
   I.useConfig(testConfigs.inlinePlayer);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
-// Scenario('Signup modal WCAG compliant', async ({ I }) => {
+// Account Pages
+
+Scenario('WCAG compliant - Account Page', async ({ I }) => {
+  I.useConfig(testConfigs.jwpAuth);
+  I.amOnPage('/u/my-account');
+  I.checkA11y();
+}).config(disableRetryFailedStep);
+
+Scenario('WCAG compliant - Payments Page', async ({ I }) => {
+  I.useConfig(testConfigs.jwpAuth);
+  I.amOnPage('/u/payments');
+  I.checkA11y();
+}).config(disableRetryFailedStep);
+
+Scenario('WCAG compliant - Favorites Page', async ({ I }) => {
+  I.useConfig(testConfigs.jwpAuth);
+  I.amOnPage('/u/favorites');
+  I.checkA11y();
+}).config(disableRetryFailedStep);
+
+
+// Modals
+
+// Scenario('Signup - Modal - WCAG compliant', async ({ I }) => {
 //   I.useConfig(testConfigs.jwpAuth);
 //   I.amOnPage('/?u=create-account');
 //   I.checkA11y();
