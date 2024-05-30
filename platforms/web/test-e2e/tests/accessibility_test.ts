5dd437afff96f9f685de90bfe25ccebea76437cf
Melissa 't Hart <melissa@videodock.com>
2024-05-30 16:24:33 +0200

    fix(a11y): add search tests and fix its heading hierarchy

--- platforms/web/test-e2e/tests/accessibility_test.ts
+++ platforms/web/test-e2e/tests/accessibility_test.ts
@@ -1,10 +1,12 @@
 import { testConfigs } from '@jwp/ott-testing/constants';
+import passwordUtils, { LoginContext } from '#utils/password_utils';
 
 Feature('Accessibility');
 
 const disableRetryFailedStep = (test: { disableRetryFailedStep: boolean }): void => {
   test.disableRetryFailedStep = true;
 };
+
 function waitForEpgAnimation(I: CodeceptJS.I, sec: number = 1) {
   I.waitForLoaderDone();
   return I.wait(sec);
@@ -38,12 +40,31 @@ Scenario('WCAG compliant - Video Detail Inline Page', async ({ I }) => {
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
+Scenario('WCAG compliant - Search Page with results', async ({ I }) => {
+  I.useConfig(testConfigs.basicNoAuth);
+  I.amOnPage('/q/Caminandes');
+  I.checkA11y();
+}).config(disableRetryFailedStep);
+
+Scenario('WCAG compliant - Search Page containing no results', async ({ I }) => {
+  I.useConfig(testConfigs.basicNoAuth);
+  I.amOnPage('/q/querywithoutresults');
+  I.checkA11y();
+}).config(disableRetryFailedStep);
+
+Scenario('WCAG compliant - Player Page', async ({ I }) => {
+  I.useConfig(testConfigs.basicNoAuth);
+  I.amOnPage('/m/awWEFyPu/big-buck-bunny?r=dGSUzs9o&play=1');
+  I.checkA11y();
+}).config(disableRetryFailedStep);
+
 Scenario('WCAG compliant - Live Page (EPG)', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/p/fWpLtzVh/?channel=Uh7zcqVm/');
   waitForEpgAnimation(I);
   I.checkA11y(null, {
     ignore: [
+      // It is a known issue that the EPG is not fully accessible due to the planby EPG package
       { id: 'scrollable-region-focusable', selector: '[class^="css-"]' },
     ],
   });
