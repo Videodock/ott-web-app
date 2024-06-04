053b2a52c6524cb6981654f6db5e8fae2dec1099
Melissa 't Hart <melissa@videodock.com>
2024-06-04 16:23:28 +0200

    chore: fix dropdown test and apply temp wait function to a11y tests

--- platforms/web/test-e2e/tests/accessibility_test.ts
+++ platforms/web/test-e2e/tests/accessibility_test.ts
@@ -1,67 +1,71 @@
 import { testConfigs } from '@jwp/ott-testing/constants';
-import passwordUtils, { LoginContext } from '#utils/password_utils';
+
+import { LoginContext } from '#utils/password_utils';
+import constants from '#utils/constants';
 
 Feature('Accessibility');
+
+let paidLoginContext: LoginContext;
 
 const disableRetryFailedStep = (test: { disableRetryFailedStep: boolean }): void => {
   test.disableRetryFailedStep = true;
 };
 
-function waitForEpgAnimation(I: CodeceptJS.I, sec: number = 1) {
-  I.waitForLoaderDone();
-  return I.wait(sec);
-}
+// The tests currently need a short `I.wait()` to ensure the pages are fully loaded before
+// performing the `checkA11y()` function. Ticket to improve this: https://videodock.atlassian.net/browse/OTT-1884
 
 Scenario('WCAG compliant - Home Page', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/');
+  I.waitForLoaderDone();
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Video Detail Page', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
+  I.wait(1.5);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Playlist Page', async ({ I }) => {
   await I.amOnPage('/p/dGSUzs9o/'); // "Films" page
-  I.checkA11y(null, {
-    ignore: [
-      { id: 'document-title', selector: 'html' },
-      { id: 'aria-valid-attr-value', selector: '[role="gridcell"]' },
-    ],
-  });
+  I.wait(1);
+  I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Video Detail Inline Page', async ({ I }) => {
   I.useConfig(testConfigs.inlinePlayer);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny');
+  I.wait(1.5);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Search Page with results', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/q/Caminandes');
+  I.wait(1);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Search Page containing no results', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/q/querywithoutresults');
+  I.wait(1);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Player Page', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/m/awWEFyPu/big-buck-bunny?r=dGSUzs9o&play=1');
+  I.wait(1);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Live Page (EPG)', async ({ I }) => {
   I.useConfig(testConfigs.basicNoAuth);
   I.amOnPage('/p/fWpLtzVh/?channel=Uh7zcqVm/');
-  waitForEpgAnimation(I);
+  I.wait(2);
   I.checkA11y(null, {
     ignore: [
       // It is a known issue that the EPG is not fully accessible due to the planby EPG package
@@ -75,18 +79,21 @@ Scenario('WCAG compliant - Live Page (EPG)', async ({ I }) => {
 Scenario('WCAG compliant - Account Page', async ({ I }) => {
   I.useConfig(testConfigs.jwpAuth);
   I.amOnPage('/u/my-account');
+  I.wait(1);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Payments Page', async ({ I }) => {
   I.useConfig(testConfigs.jwpAuth);
   I.amOnPage('/u/payments');
+  I.waitForLoaderDone();
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Favorites Page', async ({ I }) => {
   I.useConfig(testConfigs.jwpAuth);
   I.amOnPage('/u/favorites');
+  I.waitForLoaderDone();
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
@@ -95,56 +102,45 @@ Scenario('WCAG compliant - Favorites Page', async ({ I }) => {
 Scenario('WCAG compliant - Signup Modal - CLEENG', async ({ I }) => {
   I.useConfig(testConfigs.cleengAuthvod);
   I.amOnPage('/?u=create-account');
-  I.wait(0.3); // we need to wait for the modal to be fully loaded (I.seeElement was not sufficient)
+  I.waitForLoaderDone();
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Sign in Modal - CLEENG', async ({ I }) => {
   I.useConfig(testConfigs.cleengAuthvod);
   I.amOnPage('/?u=login');
-  I.wait(0.3);
+  I.wait(1);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Signup Modal - JWP', async ({ I }) => {
   I.useConfig(testConfigs.jwpAuth);
   I.amOnPage('/?u=create-account');
-  I.wait(0.3);
+  I.waitForLoaderDone();
   I.checkA11y({
-    ignore: [
-      // All form elements have an associated label.
-      // Custom fields may lack labels if they are not provided. Therefore we need this ignore
-      { id: 'label-title-only', selector: '[data-testid="crsf-input"] input' },
-    ],
+    rules: {
+      // This rule is disabled because this component does have a label prop,
+      // but is not set in the test configuration
+      'label-title-only': {
+        enabled: false,
+      },
+    },
   });
 }).config(disableRetryFailedStep);
 
 Scenario('WCAG compliant - Sign in Modal - JWP', async ({ I }) => {
   I.useConfig(testConfigs.jwpAuth);
   I.amOnPage('/?u=login');
-  I.wait(0.3);
+  I.wait(1);
   I.checkA11y();
 }).config(disableRetryFailedStep);
 
-const loginContext: LoginContext = {
-  email: passwordUtils.createRandomEmail(),
-  password: passwordUtils.createRandomPassword(),
-};
+Scenario('WCAG compliant - Choose Offer Modal', async ({ I }) => {
+  I.useConfig(testConfigs.svod);
+  paidLoginContext = await I.registerOrLogin(paidLoginContext);
+  I.amOnPage(constants.offersUrl);
 
-Scenario.skip('WCAG compliant - Choose Offer Modal', async ({ I }) => {
-  I.useConfig(testConfigs.cleengAuthvod);
-  I.amOnPage('/?u=create-account');
+  I.wait(1.5);
 
-  await I.fillRegisterForm(loginContext);
-  I.wait(10);
-  I.seeElement('[role="dialog"]');
-
-  I.checkA11y({
-    ignore: [
-      // We are of the opinion that a heading 2 is descriptive enough for the Dialogs and think that
-      // adding an aria-label etc, is redundant
-      { id: 'aria-dialog-name', selector: 'div[role="dialog"]' }
-    ],
-  });
+  I.checkA11y();
 }).config(disableRetryFailedStep);
-
